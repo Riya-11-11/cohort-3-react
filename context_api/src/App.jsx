fa474b907f75b8775e8ff956903701b1c37ca270
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-07-17 11:37:27 +0530

    feat: add ArrContext and update App component to utilize context values

--- context_api/src/App.jsx
+++ context_api/src/App.jsx
@@ -3,17 +3,33 @@ import { useContext } from "react";
 import { LanguageContext } from "./context/LanguageContext";
 import { ThemeContext } from "./context/ThemeContext";
 import { MyStore } from "./context/StoreContext";
+import { ArrContext } from "./context/ArrayContext";
 
 const App = () => {
   const language = useContext(LanguageContext);
   const theme = useContext(ThemeContext);
-  const user = useContext(MyStore)
+  // const user = useContext(MyStore);
+  const { name, age, city, college, semester } = useContext(MyStore); //destructure way
+  const arr = useContext(ArrContext);
   return (
     <div>
       <h2>Theme: {theme}</h2>
-      <h3>Name: {user.name}</h3>
-      <h3>Age: {user.age}</h3>
+      <h3>Name: {name}</h3>
+      <h3>Age: {age}</h3>
+      <h3>City: {city}</h3>
+      <h3>College: {college}</h3>
+      <h3>Semester: {semester}</h3>
       <h3>Current Language: {language}</h3>
+      {/* <h3>
+        {arr.map((val) => {
+          console.log(val);
+        })}
+      </h3> */}
+      <h3>
+        {arr.map((val, index) => (
+          <p key={index}>{val}</p>
+        ))}
+      </h3>
     </div>
   );
 };
--- context_api/src/main.jsx
+++ context_api/src/main.jsx
@@ -4,12 +4,23 @@ import App from "./App.jsx";
 import { LanguageContext } from "./context/LanguageContext.jsx";
 import { ThemeContext } from "./context/ThemeContext.jsx";
 import { MyStore } from "./context/StoreContext.jsx";
+import { ArrContext } from "./context/ArrayContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <LanguageContext.Provider value="English">
     <ThemeContext.Provider value="Light">
-      <MyStore.Provider value={{name:"Riya", age:"20"}}>
-            <App />
+      <MyStore.Provider
+        value={{
+          name: "Riya",
+          age: 20,
+          city: "Kolkata",
+          college: "IIT",
+          semester: 4,
+        }}
+      >
+        <ArrContext.Provider value={["React", "Node", "MongoDB"]}>
+          <App />
+        </ArrContext.Provider>
       </MyStore.Provider>
     </ThemeContext.Provider>
   </LanguageContext.Provider>,
