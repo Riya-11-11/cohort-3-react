62d38cc4900280540a4471303241694f8dd46d61
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-07-19 15:38:30 +0530

    feat: enhance CartItem component with item details and integrate into Cart page

--- shopping-cart-contextApi/src/components/CartItem.jsx
+++ shopping-cart-contextApi/src/components/CartItem.jsx
@@ -1,9 +1,10 @@
 import { Trash2 } from "lucide-react";
 
 function CartItem({ item }) {
+  console.log(item );
+
   return (
     <div className="bg-white rounded-xl shadow p-4 flex flex-col md:flex-row gap-5">
-
       <img
         src={item.image}
         alt={item.name}
@@ -11,45 +12,26 @@ function CartItem({ item }) {
       />
 
       <div className="flex-1">
+        <h2 className="text-2xl font-semibold">{item.name}</h2>
 
-        <h2 className="text-2xl font-semibold">
-          {item.name}
-        </h2>
+        <p className="text-slate-500 mt-2">{item.description}</p>
 
-        <p className="text-slate-500 mt-2">
-          {item.description}
-        </p>
-
-        <h3 className="text-blue-600 text-2xl font-bold mt-4">
-          ₹{item.price}
-        </h3>
-
+        <h3 className="text-blue-600 text-2xl font-bold mt-4">₹{item.price}</h3>
       </div>
 
       <div className="flex flex-col justify-between items-end">
-
         <button className="text-red-500 hover:text-red-700">
           <Trash2 />
         </button>
 
         <div className="flex items-center gap-3">
+          <button className="bg-slate-200 w-9 h-9 rounded-lg">-</button>
 
-          <button className="bg-slate-200 w-9 h-9 rounded-lg">
-            -
-          </button>
+          <span className="font-semibold">1</span>
 
-          <span className="font-semibold">
-            1
-          </span>
-
-          <button className="bg-slate-200 w-9 h-9 rounded-lg">
-            +
-          </button>
-
+          <button className="bg-slate-200 w-9 h-9 rounded-lg">+</button>
         </div>
-
       </div>
-
     </div>
   );
 }
--- shopping-cart-contextApi/src/pages/Cart.jsx
+++ shopping-cart-contextApi/src/pages/Cart.jsx
@@ -1,20 +1,21 @@
-import { useContext } from "react";
 import CartItem from "../components/CartItem";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 
 function Cart() {
   const { cart } = useContext(CartContext);
-  function CartItem({item}) {
-    
-  }
+  console.log("Cart Page:", cart);
+
   return (
     <div className="max-w-7xl mx-auto px-6 py-10">
       <h1 className="text-3xl font-bold mb-8">My Cart</h1>
 
       <div className="grid lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2 space-y-5">
-          <CartItem item={item} />
+          {cart.map((item) => {
+            //return needed in->{}, not need of return in ->()
+            return <CartItem key={item.id} item={item} />;
+          })}
         </div>
 
         <div className="bg-white rounded-xl shadow p-6 h-fit">
