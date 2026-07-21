cd0d60586de7ac4521ce51afecd882e1c1dbd9b1
Riya-11-11 <riyasingh7542.com@gmail.com>
2026-07-21 12:36:07 +0530

    refactor: streamline addToCart function and enhance logging in CartContext

--- shopping-cart-contextApi/src/context/CartContext.jsx
+++ shopping-cart-contextApi/src/context/CartContext.jsx
@@ -4,27 +4,24 @@ export const CartContext = createContext();
 
 export function CartContextProvider({ children }) {
   const [cart, setCart] = useState([]);
+  console.log("Cart State:", cart);
 
- const addToCart = (product) => {
-  const productItem = cart.find(
-    (item) => item.id === product.id
-  );
+  const addToCart = (product) => {
+    console.log("Clicked Product:", product);
+    const productItem = cart.find((item) => item.id === product.id);
 
-  if (productItem) {
-    setCart((prev) =>
-      prev.map((item) =>
-        item.id === product.id
-          ? { ...item, quantity: item.quantity + 1 }
-          : item
-      )
-    );
-  } else {
-    setCart((prev) => [
-      ...prev,
-      { ...product, quantity: 1 }
-    ]);
-  }
-};
+    if (productItem) {
+      setCart((prev) =>
+        prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        ),
+      );
+    } else {
+      setCart((prev) => [...prev, { ...product, quantity: 1 }]);
+    }
+  };
 
   return (
     <CartContext.Provider value={{ addToCart, cart }}>
